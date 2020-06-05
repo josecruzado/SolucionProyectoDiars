c31af4244065685c9ebe6c49b07ac2b4c0d8e334
Gustavo Escobar <N00077723@upn.pe>
2020-06-05 12:36:15 -0500

    Periodo de pago SET Y GET

--- CapaDominio/Entidades/PeriodoDePago.cs
+++ CapaDominio/Entidades/PeriodoDePago.cs
@@ -13,6 +13,46 @@ namespace CapaDominio.Entidades
         private DateTime fechaInicio { get; set;}
         private int semanasDePeriodo { get; set; }
 
+        public void setEstado(Boolean estado)
+        {
+            this.estado=estado;
+        }
+
+        public Boolean getEstado()
+        {
+            return estado;
+        }
+
+        public void setFechaFin(DateTime fechaFin)
+        {
+            this.fechaFin=fechaFin;
+        }
+
+        public DateTime getFechaFin()
+        {
+            return fechaFin;
+        }
+
+        public void setFechaInicio(DateTime fechaInicio)
+        {
+            this.fechaInicio =fechaInicio;
+        }
+
+        public DateTime getFechaInicio()
+        {
+            return fechaInicio;
+        }
+
+        public void setSemanasDePeriodo(int semanasDePeriodo)
+        {
+            this.semanasDePeriodo = semanasDePeriodo;
+        }
+
+        public int getSemanasDePeriodo()
+        {
+            return semanasDePeriodo;
+        }
+
         public Boolean sePuedeProcesar() 
         {
         
