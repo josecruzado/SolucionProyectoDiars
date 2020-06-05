b2afff33d71ca9244352ca027168ca555e846857
Gustavo Escobar <N00077723@upn.pe>
2020-06-05 10:39:05 -0500

    14

--- CapaDominio/Entidades/Boleta.cs
+++ CapaDominio/Entidades/Boleta.cs
@@ -15,51 +15,10 @@ namespace CapaDominio.Entidades
         private int totalDeHoras;
         private double totalDescuentos;
         private double totalIngresos;
-        private Contrato contrato;
 
-        public void calcularAsignacionFamiliar()
-        {
-            if(contrato.getTieneAsignacionFamiliar == true)
-            {
-                asignacionFamiliar = 940 * 0.1;
-            }
-            else
-            {
-                asignacionFamiliar = 0;
-            }
-        }
 
-        public double calcularDescuentoAfp()
-        {
-            return afp.getPorcentajeDescuentoAFP()  *  sueldoBasico;
-        }
+    
 
-        public void calcularSueldoBasico()
-        {
-            sueldoBasico = totalDeHoras * valorHora;
-        }
-
-        public void calcularSueldoNeto()
-        {
-            
-        }
-
-        public void calcularTotalDeDescuento()
-        {
-
-        }
-
-        public void calcularTotalDeHoras()
-        {
-
-            totalDeHoras = totalHorasPorSemana * conceptos.periodoDePago.semanaDePeriodo;
-               
-        }
-
-        public void calcularTotalDeIngreso()
-        {
-
-        }
 
     }
 }
